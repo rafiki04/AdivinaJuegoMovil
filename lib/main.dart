c3fbfc746163cf0c2ae3c03e56e24c9b5eb6a006
rafiki04 <cris_rgj13@hotmail.com>
2026-04-27 21:22:02 -0600

    haciendo WelcomePage pagina principal y modificando el main

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:audioplayers/audioplayers.dart'; // ← nuevo
+
+import 'package:game/features/guess_game/domain/usecases/MakeGuess.dart';
+import 'package:game/features/guess_game/domain/usecases/ResetGame.dart';
+import 'package:game/features/guess_game/presentation/state/GameProvider.dart';
+import 'package:game/features/guess_game/presentation/pages/GamePage.dart';
+import 'package:game/features/guess_game/presentation/pages/WelcomePage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // ← nuevo
+  await AudioPlayer.global.setGlobalAudioContext(
+    AudioContext(
+      android: AudioContextAndroid(
+        audioFocus: AndroidAudioFocus.gain,
+        usageType: AndroidUsageType.game,
+      ),
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GameProvider(
+        MakeGuess(),
+        ResetGame(),
+      )..changeDifficulty(10, 5),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomePage(),
+      ),
+    );
+  }
+}
